abce83f1ceb70544c9ea8da9f84ac0e62c4eee75
Adam Al-Hosam <adam.alhosam@icloud.com>
2024-12-29 15:01:05 +0100

    Add test for ignoring partial match in magic number

--- big/2/public-tests/tests/transfer.rs
+++ big/2/public-tests/tests/transfer.rs
@@ -282,3 +282,47 @@ async fn test_serialization_errors() {
         _ => panic!("Expected Read command"),
     }
 }
+
+#[tokio::test]
+#[timeout(200)]
+async fn test_magic_prefix_partial_match_is_ignored() {
+    // given
+    let request_identifier = 7;
+    let sector_idx = 8;
+    let register_cmd = RegisterCommand::Client(ClientRegisterCommand {
+        header: ClientCommandHeader {
+            request_identifier,
+            sector_idx,
+        },
+        content: ClientRegisterCommandContent::Read,
+    });
+    let mut sink: Vec<u8> = Vec::new();
+
+    // add first 3 bytes from MAGIC_NUMBER
+    sink.extend_from_slice(&MAGIC_NUMBER.as_slice()[..3]);
+
+    // when
+    serialize_register_command(&register_cmd, &mut sink, &[0x00_u8; 32])
+        .await
+        .expect("Could not serialize?");
+    let mut slice: &[u8] = &sink[..];
+    let data_read: &mut (dyn tokio::io::AsyncRead + Send + Unpin) = &mut slice;
+
+    let (deserialized_cmd, hmac_valid) =
+        deserialize_register_command(data_read, &[0x00_u8; 64], &[0x00_u8; 32])
+            .await
+            .expect("Could not deserialize");
+
+    // then
+    assert!(hmac_valid);
+    match deserialized_cmd {
+        RegisterCommand::Client(ClientRegisterCommand {
+            header,
+            content: ClientRegisterCommandContent::Read,
+        }) => {
+            assert_eq!(header.sector_idx, sector_idx);
+            assert_eq!(header.request_identifier, request_identifier);
+        }
+        _ => panic!("Expected Read command"),
+    }
+}
